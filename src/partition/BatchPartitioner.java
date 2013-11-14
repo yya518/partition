d6fc57a0fe03f472612f01c4452ed1b2d739c044
Yi Yang <yyyn@amazon.com>
2013-11-14 01:57:11 +0000

    add batch partitioner

---
+++ src/partition/BatchPartitioner.java
@@ -0,0 +1,14 @@
+package partition;
+
+public class BatchPartitioner extends Partitioner {
+
+	public BatchPartitioner(Corpus corpus, int splits) {
+		super(corpus, splits);
+	}
+
+	@Override
+	public void partition() {
+
+	}
+
+}
--- src/partition/PartitionerFactory.java
+++ src/partition/PartitionerFactory.java
@@ -5,6 +5,10 @@ public class PartitionerFactory{
 	public static Partitioner getPartitioner(String name, Corpus corpus, int splits){
 		if (name.equals("random"))
 			return new RandomPartitioner(corpus, splits);
+		
+		else if (name.equals("batch"))
+			return new BatchPartitioner(corpus, splits);
+		
 		else {
 			throw new IllegalArgumentException(name + "is not a valid partitioner name");
 		}
